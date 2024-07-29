14354142f295f834e6c2c7795d25822a4a0a80ec
Jeonghoon2 <ukidd12@naver.com>
2024-07-29 09:39:04 +0900

    list2df

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -1,39 +1,34 @@
 import requests
 import os
+import pandas as pd
 
-def req2dataframe():
-	_, data = req()
-	l = data['boxOfficeResult']['dailyBoxOfficeList']
-	retun l
-	
+def list2df():
+    l = req2list()
+    df = pd.DataFrame(l)
+    return df
+
+def req2list() -> list:
+    _, data = req()
+    l = data['boxOfficeResult']['dailyBoxOfficeList']
+    return l
+
 def get_key():
-	key = os.getenv('MOVIE_API_KEY')
-	return key
+    key = os.getenv('MOVIE_API_KEY')
+    return key
 
-def gen_req_url(dt="20120101"):
-	base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml "
-	key = "6158692ade44de1857753bae9741d704"
+def req(dt="20120101"):
+    #url = gen_url('20240720')
+    url = gen_url(dt)
+    r = requests.get(url)
+    code = r.status_code
+    data = r.json()
+    print(data)
+    return code, data
 
-	url = f"{base_url}?key={key}&targetDt={dt}"
-	return url
 
-def req(url:str):
-	res = request.get(url)
+def gen_url(dt="20120101"):
+    base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+    key = get_key()
+    url = f"{base_url}?key={key}&targetDt={dt}"
 
-	code = res.status_code()
-	data = res.json()
-
-	return [data, code] 
-
-def main():
-	print("start")
-	url = gen_req_url
-	res = req(url)
-
-	print(f"""
-	{res[0]}
-
-	{res[1]}
-
-	"""
-	)
+    return url
