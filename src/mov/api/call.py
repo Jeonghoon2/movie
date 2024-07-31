c2fdd392ce95b386519b05b3bab786608cdb1c58
Jeonghoon2 <ukidd12@naver.com>
2024-07-31 19:16:42 +0900

    Partion Fix

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -24,21 +24,26 @@ def apply_type2df(load_dt="20120101", path="~/tmp/test_parquet"):
 
 
 def save2df(load_dt='20120101', url_params={}):
-    """airflow 호출 지점"""
+
     df = list2df(load_dt, url_params)
+
     # df 에 load_dt 컬럼 추가 (조회 일자 YYYYMMDD 형식 으로)
-    # 아래 파일 저장시 load_dt 기분으로 파티셔닝
+    # 아래 파일 저장시 load_dt 기준 으로 파티셔닝
     df['load_dt'] = load_dt
 
-    partition = [
+    # Partitioning List
+    partitions = [
         'load_dt'
     ]
 
     for k, v in url_params.items():
         df[k] = v
-        partition.append(k)
+        partitions.append(k)
 
-    df.to_parquet('~/tmp/test_parquet', partition_cols=partition)
+    print(f"partitioning List : {partitions}")
+
+    df.to_parquet('~/tmp/test_parquet', partition_cols=partitions)
+
     return df
 
 
